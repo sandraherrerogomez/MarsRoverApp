82b0caa2201ad2cf00e63a5c99675f9ddd546ec6
Sandra Herrero Gomez <sandra.herrero.gomez@autodesk.com>
2020-01-11 14:58:27 +0000

    first working prototype

--- src/main/java/MarsRoverApplication.java
+++ src/main/java/MarsRoverApplication.java
@@ -1,7 +1,17 @@
 public class MarsRoverApplication {
     public static void main (String args[]) {
-        MarsRovers rover=new MarsRovers("1 5 N");
-        System.out.println(rover.getCoords());
+        MarsRovers rover=new MarsRovers("3 3 E", "1 1");
+
+        try {
+            rover.move("MMRMMRMRRM");
+            System.out.println(rover.getCoords());
+            System.out.println(rover.getOrientation());
+        }
+
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
 
--- src/main/java/MarsRovers.java
+++ src/main/java/MarsRovers.java
@@ -4,6 +4,8 @@ public class MarsRovers {
 
     private Coordinate coords;
     private Orientation orientation;
+    private int limitX;
+    private int limitY;
 
     public Coordinate getCoords() {
         return coords;
@@ -23,37 +25,117 @@ public class MarsRovers {
 
 
     // 1 5 N
-    public MarsRovers(String originCoordOrientation){
+    public MarsRovers(String originCoordOrientation, String plateauLimits) {
+        this.limitX = Character.getNumericValue(plateauLimits.charAt(0));
+        this.limitY = Character.getNumericValue(plateauLimits.charAt(2));
+
         int x = Character.getNumericValue(originCoordOrientation.charAt(0));
-        int y= Character.getNumericValue(originCoordOrientation.charAt(2));;
-        this.coords=new Coordinate(x,y);
+        int y = Character.getNumericValue(originCoordOrientation.charAt(2));
+        this.coords = new Coordinate(x, y);
 
-        switch (originCoordOrientation.charAt(4)){
-            case 'N': this.orientation = Orientation.NORTH;
-            break;
-            case 'S': this.orientation = Orientation.SOUTH;
-            break;
-            case 'E': this.orientation = Orientation.EAST;
-            break;
-            case 'W': this.orientation = Orientation.WEST;
-            break;
+        switch (originCoordOrientation.charAt(4)) {
+            case 'N':
+                this.orientation = Orientation.NORTH;
+                break;
+            case 'S':
+                this.orientation = Orientation.SOUTH;
+                break;
+            case 'E':
+                this.orientation = Orientation.EAST;
+                break;
+            case 'W':
+                this.orientation = Orientation.WEST;
+                break;
         }
 
-        if(originCoordOrientation.charAt(4)=='N'){
-            this.orientation=Orientation.NORTH;
-        }
-        else if(originCoordOrientation.charAt(4)=='S'){
+        if (originCoordOrientation.charAt(4) == 'N') {
+            this.orientation = Orientation.NORTH;
+        } else if (originCoordOrientation.charAt(4) == 'S') {
 
         }
     }
 
+    // LMLMLMLMM L=left R=right M=forward
+    public void move(String orders) throws Exception{
+        for (int i = 0; i < orders.length(); i++) {
 
-    public void move(String orders) {
+            switch (orders.charAt(i)) {
+                case 'L': {
+                    switch (orientation) {
+                        case NORTH:
+                            orientation = Orientation.WEST;
+                            break;
+                        case WEST:
+                            orientation = Orientation.SOUTH;
+                            break;
+                        case SOUTH:
+                            orientation = Orientation.EAST;
+                            break;
+                        case EAST:
+                            orientation = Orientation.NORTH;
+                            break;
+                    }
+
+
+                }
+                break;
+                case 'R': {
+                    switch (orientation) {
+                        case NORTH:
+                            orientation = Orientation.EAST;
+                            break;
+                        case WEST:
+                            orientation = Orientation.NORTH;
+                            break;
+                        case SOUTH:
+                            orientation = Orientation.WEST;
+                            break;
+                        case EAST:
+                            orientation = Orientation.SOUTH;
+                            break;
+                    }
+
+
+                }
+                break;
+                case 'M': {
+                    switch (orientation) {
+                        case NORTH:
+                            if(coords.getY()+1>limitY){
+                                throw new Exception("WARNING!!! WE HAVE LOST THE ROVER!!!!!! THIS SEEMS EXPENSIVE... :(");
+                        }
+                            coords.setY(coords.getY() + 1);
+                            break;
+                        case WEST:
+                            if(coords.getX()-1<0){
+                                throw new Exception("WARNING!!! WE HAVE LOST THE ROVER!!!!!! THIS SEEMS EXPENSIVE... :(");
+                            }
+                            coords.setX(coords.getX() - 1);
+                            break;
+                        case SOUTH:
+                            if(coords.getY()-1<0){
+                            throw new Exception("WARNING!!! WE HAVE LOST THE ROVER!!!!!! THIS SEEMS EXPENSIVE... :(");
+                        }
+                            coords.setY(coords.getY() - 1);
+                            break;
+                        case EAST:
+                            if(coords.getX()+1>limitX){
+                            throw new Exception("WARNING!!! WE HAVE LOST THE ROVER!!!!!! THIS SEEMS EXPENSIVE... :(");
+                        }
+                            coords.setX(coords.getX() + 1);
+                            break;
+                    }
+
+                }
+
+            }
+
+        }
 
     }
-
 }
 
- enum Orientation {
+enum Orientation {
     NORTH, SOUTH, EAST, WEST
-};
+}
+
